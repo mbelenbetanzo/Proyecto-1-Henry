929cb4546ce7e0043e9840d44e3c495edaf94c0c
mbelen <beel.betanzoo@hotmail.com.ar>
2024-05-29 20:42:52 -0300

    haciendo js

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
 
+//llamar a elementos del dom
+
+const form = document.getElementById("form");
+const container = document.getElementById("container");
+
 // CLASE ACTIVITY //
 
 class Activity {
-    constructor(id,title,description,imgUrl){
+    constructor({id,title,description,imgUrl}){
         this.id = id;
         this.title = title;
         this.description = description;
@@ -10,15 +15,12 @@ class Activity {
     }
 }
 
-const activity = new Activity(1,"correr","correr 2k por dia","www.#.com")
-console.log(activity);
-
 // CLASE REPOSITORY //
 
 class Repository {
     constructor() {
         this.activities = []; // Un arreglo para almacenar las actividades.
-        this.nextId = 1; // Inicializamos un contador para los IDs.
+        this.porId = 1; // Inicializamos un contador para los IDs.
     }
 
     getAllActivities() { // Retorna un arreglo con todas las actividades.
@@ -26,29 +28,44 @@ class Repository {
     }
       
     createActivity(title, description, imgUrl) { // Crea una instancia de Activity y la almacena en el arreglo.
-        const activity = new Activity(this.nextId++, title, description, imgUrl);
+        const activity = new Activity(title, description, imgUrl); //this.porId++,
         this.activities.push(activity);
+        this.porId++
     }
-
     deleteActivity(id) {
-        this.activities = this.activities.filter((item) => item.id !== id);
+        this.activities  = this.activities.filter((item) => item.id !== id);
     }
 }
 
 const repo = new Repository();
-
-// Usar los métodos de la instancia repo
 repo.createActivity("correr", "hacer ejercicio", "www.ejemplo.com/correr.jpg");
 
-console.log(repo.getAllActivities());
-// [{ id: 1, title: "correr", description: "hacer ejercicio", imgUrl: "www.ejemplo.com/correr.jpg" },
-//  { id: 2, title: "holi", description: "celebración de colores", imgUrl: "www.ejemplo.com/holi.jpg" }]
+const activity = new Activity({
+    id: 1,
+    title: "correr",
+    description:"correr 2k por dia",
+    imgUrl: "www.#.com"})
 
+//funcion punto uno, recibir por parametro la instancia de actividad
+const convertirHtml = (activity) => {
+    const { title, description, imgUrl } = activity;
+    const card = document.createElement("div");
+    const titulo = document.createElement("h3")
+    const descripcion = document.createElement("p");
+    const imagen = document.createElement("img");
+    titulo.innerText = title;
+    descripcion.innerText = description;
+    imagen.src = imgUrl;
+    card.classList.add("card");
+    titulo.classList.add("titulo")
+    descripcion.classList.add("descripcion")
+    imagen.classList.add("imagen")
+    card.appendChild(titulo)
+    card.appendChild(descripcion)
+    card.appendChild(imagen)
+    return card;
+}
 
-repo.deleteActivity(1);
-console.log(repo.getAllActivities());
-// [{ id: 2, title: "holi", description: "celebración de colores", imgUrl: "www.ejemplo.com/holi.jpg" },
-//  { id: 3, title: "leer", description: "leer un libro", imgUrl: "www.ejemplo.com/leer.jpg" }]
 
 
 
